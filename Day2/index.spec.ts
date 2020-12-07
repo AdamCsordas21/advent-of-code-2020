47602f2598ee890d8287427d7d8c64711e63dbe6
AdamCsordas21 <adamcsordas@hotmail.com>
2020-12-07 20:35:18 +0000

    Validates passwords

--- Day2/index.spec.ts
+++ Day2/index.spec.ts
@@ -1,15 +1,23 @@
-import { input2lines } from ".";
+import { input2lines, isValid } from ".";
 
 describe("validate password", () => {
-  it("validates password", () => {
+  it("splits input into lines", () => {
     const input = `1-3 a: abcde
 1-3 b: cdefg
 2-9 c: ccccccccc`;
 
     expect(input2lines(input)).toEqual([
-      '1-3 a: abcde',
-      '1-3 b: cdefg',
-      '2-9 c: ccccccccc',
+      "1-3 a: abcde",
+      "1-3 b: cdefg",
+      "2-9 c: ccccccccc",
     ]);
+  });
+
+  it.each<[string, boolean]>([
+    ["1-3 a: abcde", true],
+    ["1-3 b: cdefg", false],
+    ["2-9 c: ccccccccc", true],
+  ])("validates password %s => %s", (line, shouldBeValid) => {
+    expect(isValid(line)).toEqual(shouldBeValid);
   });
 });
--- Day2/index.ts
+++ Day2/index.ts
@@ -1,1 +1,13 @@
 export const input2lines = (input: string): string[] => input.split("\n");
+
+export const isValid = (line: string): boolean => {
+  const [range, required, password] = line.split(" ");
+  const [min, max] = range.split("-");
+
+  let count = 0;
+  for (const char of password) {
+    if (char === required.charAt(0)) count++;
+  }
+
+  return count >= +min && count <= +max;
+};
