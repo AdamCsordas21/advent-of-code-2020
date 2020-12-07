d1507cff9ce9f07a62bfd89ec38fb988ec71eb7f
AdamCsordas21 <adamcsordas@hotmail.com>
2020-12-07 19:54:16 +0000

    Refactor find2020 & find20202of3 functions

--- Day1/index.ts
+++ Day1/index.ts
@@ -1,9 +1,9 @@
 export const find2020 = (input: number[]): [number, number] => {
   for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length; j++) {
+    for (let j = i + 1; j < input.length; j++) {
       const first = input[i];
       const second = input[j];
-      if (i !== j && first + second === 2020) return [first, second];
+      if (first + second === 2020) return [first, second];
     }
   }
   return [0, 0];
@@ -11,12 +11,12 @@ export const find2020 = (input: number[]): [number, number] => {
 
 export const find2020of3 = (input: number[]): [number, number, number] => {
   for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length; j++) {
-      for (let k = 0; k < input.length; k++) {
+    for (let j = i + 1; j < input.length; j++) {
+      for (let k = j + 1; k < input.length; k++) {
         const first = input[i];
         const second = input[j];
         const third = input[k];
-        if (i !== j && i !== k && k !== j && first + second + third === 2020) return [first, second, third];
+        if (first + second + third === 2020) return [first, second, third];
       }
     }
   }
