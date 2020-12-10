1e6a8652862d2803b6a2ebfa86253dd960b33346
AdamCsordas21 <adamcsordas@hotmail.com>
2020-12-10 19:34:23 +0000

    Counts valid passwords

--- Day1/index.ts
+++ Day1/index.ts
@@ -1,25 +1,24 @@
 export const find2020 = (input: number[]): [number, number] => {
   for (let i = 0; i < input.length; i++) {
     for (let j = i + 1; j < input.length; j++) {
-      const first = input[i];
-      const second = input[j];
-      if (first + second === 2020) return [first, second];
+      const first = input[i]
+      const second = input[j]
+      if (first + second === 2020) return [first, second]
     }
   }
-  return [0, 0];
-};
+  return [0, 0]
+}
 
 export const find2020of3 = (input: number[]): [number, number, number] => {
   for (let i = 0; i < input.length; i++) {
     for (let j = i + 1; j < input.length; j++) {
       for (let k = j + 1; k < input.length; k++) {
-        const first = input[i];
-        const second = input[j];
-        const third = input[k];
-        if (first + second + third === 2020) return [first, second, third];
+        const first = input[i]
+        const second = input[j]
+        const third = input[k]
+        if (first + second + third === 2020) return [first, second, third]
       }
     }
   }
-  return [0, 0, 0];
-};
-
+  return [0, 0, 0]
+}
--- Day2/index.ts
+++ Day2/index.ts
@@ -1,13 +1,15 @@
-export const input2lines = (input: string): string[] => input.split("\n");
+export const input2lines = (input: string): string[] => input.split('\n')
 
 export const isValid = (line: string): boolean => {
-  const [range, required, password] = line.split(" ");
-  const [min, max] = range.split("-");
+  const [range, required, password] = line.split(' ')
+  const [min, max] = range.split('-')
 
-  let count = 0;
+  let count = 0
   for (const char of password) {
-    if (char === required.charAt(0)) count++;
+    if (char === required.charAt(0)) count++
   }
 
-  return count >= +min && count <= +max;
-};
+  return count >= +min && count <= +max
+}
+
+export const validPasswords = (input: string): number => input2lines(input).map(isValid).filter(v => v).length
