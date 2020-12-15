2b60d31387dc0a10adb93f0e6d3dbcd287e5f83f
AdamCsordas21 <adamcsordas@hotmail.com>
2020-12-15 20:57:24 +0000

    Get valid passport

--- Day4/index.spec.ts
+++ Day4/index.spec.ts
@@ -1,4 +1,4 @@
-import { input2passports } from './index'
+import { input2passports, validateFields } from './index'
 
 describe('passport processing', () => {
   const sampleInput = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
@@ -22,4 +22,13 @@ iyr:2011 ecl:brn hgt:59in`
       'hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm',
       'hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in',
     ])})
+
+  it.each<[passport: string, expected: boolean]>([
+    ['ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm', true],
+    ['iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929', false],
+    ['hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm', true],
+    ['hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in', false],
+  ])('validates passport field', (passport, expected) => {
+    expect(validateFields(passport)).toEqual(expected)
+  })
 })
--- Day4/index.ts
+++ Day4/index.ts
@@ -1,3 +1,18 @@
 export const input2passports = (input: string): string[] => input
   .split('\n\n')
   .map(passport => passport.replace(/\n/g, ' '))
+
+const requiredFields = [
+  'byr',
+  'iyr',
+  'eyr',
+  'hgt',
+  'hcl',
+  'ecl',
+  'pid',
+]
+export const validateFields = (passport: string): boolean =>
+  requiredFields
+    .map(requiredField => passport.split(' ').map(field => field.substring(0, 3)).includes(requiredField))
+    .filter(f => !!f)
+    .length === requiredFields.length
