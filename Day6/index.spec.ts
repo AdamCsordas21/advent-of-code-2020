0fc7e3ff33cbd303ab4ab88c689a7cac68cf722e
AdamCsordas21 <adamcsordas@hotmail.com>
2021-01-14 23:06:37 +0000

    Split group into unique answers

--- Day6/index.spec.ts
+++ Day6/index.spec.ts
@@ -1,5 +1,7 @@
 import {
+  countUniqueYesAnswers,
   Group,
+  groups2UniqueYesAnswers,
   input2groups,
   sumUniqueAnswersAcrossGroups,
 } from '.'
@@ -30,6 +32,39 @@ b`
       'b'
     ]
     expect(input2groups(input)).toEqual(expected)
+  })
+
+  it('splits group into unique answers', () => {
+    const input = 'abc\na'
+    const expected = 'abc'
+
+    /*
+    let uniqueAnswers1 = []
+    for (const c of input)
+      if (c !== '\n' && !uniqueAnswers1.includes(c)) uniqueAnswers1.push(c)
+
+    const x = new Set()
+    for (const ans of input.split('').filter(c => c !== '\n'))
+      x.add(ans)
+    const a1 = [...x]
+
+    let uniqueAnswers2 = ''
+    for (const c of input)
+      if (c !== '\n' && !uniqueAnswers2.includes(c)) uniqueAnswers2 += c
+   
+
+    const nonUnique = input.split('') // ['a', 'b', 'c', '\n', 'a']
+      .filter(c => c !== '\n') // ['a', 'b', 'c', 'a']
+    const a = [...new Set(nonUnique)]
+*/
+
+expect(groups2UniqueYesAnswers(input)).toEqual(expected)
+  })
+
+  it.skip('counts unique yes answers in a group', () => {
+    const input = 'abc'
+    const expected = 3
+    expect(countUniqueYesAnswers(input)).toEqual(expected)
   })
 
   it.skip('part 1', () => {
--- Day6/index.ts
+++ Day6/index.ts
@@ -1,12 +1,35 @@
 // 26 yes-or-no questions marked from A-Z
 
 export type Group = string
-export const input2groups = (input: string): Group[] => {
-  return input.split('\n\n')
+export type UniqueAnswers = string
+
+export const groups2UniqueYesAnswers = (input: Group): UniqueAnswers => {
+  let ans: UniqueAnswers = ''
+  for (const char of input) {
+    if (char !== '\n' && !ans.includes(char)) ans += char
+  }
+  return ans
 }
+
+export const countUniqueYesAnswers = (input: Group): number => {
+  return 0
+}
+
+export const input2groups = (input: string): Group[] => input.split('\n\n')
 
 export const sumUniqueAnswersAcrossGroups = (input: string): number => {
   // 3+3+3+1+1 -> this suggests that we need to split the input into groups
   const groups = input2groups(input)
+  // [ 'abc', 'a\nb\nc', 'ab\nac', 'a\na\na\na', 'b' ] -> 3+3+3+1+1
+  // 'abc' -> 3; 'a\nb\nc' -> 3;...
+
+  // group with multiple people
+  // groups   u ans   count
+  // a b c -> abc   => 3 
+  // ab ab -> ab    => 2 
+  // ab bc -> abc   => 3 
+  // ab cd -> abcd  => 4 
+
+  
   return 0
 }
