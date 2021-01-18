c982f510e2e34bcd0f9c767f820b5e855f2fce48
AdamCsordas21 <adamcsordas@hotmail.com>
2021-01-18 20:03:39 +0000

    Add sumUniqueAnswersAcrossGroups

--- Day6/index.ts
+++ Day6/index.ts
@@ -3,6 +3,8 @@
 export type Group = string
 export type UniqueAnswers = string
 
+export const input2groups = (input: string): Group[] => input.split('\n\n')
+
 export const groups2UniqueYesAnswers = (input: Group): UniqueAnswers => {
   let ans: UniqueAnswers = ''
   for (const char of input) {
@@ -11,18 +13,13 @@ export const groups2UniqueYesAnswers = (input: Group): UniqueAnswers => {
   return ans
 }
 
-export const countUniqueYesAnswers = (input: Group): number => {
-  return 0
-}
+export const countUniqueYesAnswers = (input: UniqueAnswers): number => input.length
 
-export const input2groups = (input: string): Group[] => input.split('\n\n')
+export const sum = (numbers: number[]): number => numbers.reduce((acc, number) => acc + number)
 
 export const sumUniqueAnswersAcrossGroups = (input: string): number => {
   // 3+3+3+1+1 -> this suggests that we need to split the input into groups
   const groups = input2groups(input)
-  // [ 'abc', 'a\nb\nc', 'ab\nac', 'a\na\na\na', 'b' ] -> 3+3+3+1+1
-  // 'abc' -> 3; 'a\nb\nc' -> 3;...
-
   // group with multiple people
   // groups   u ans   count
   // a b c -> abc   => 3 
@@ -30,6 +27,14 @@ export const sumUniqueAnswersAcrossGroups = (input: string): number => {
   // ab bc -> abc   => 3 
   // ab cd -> abcd  => 4 
 
+  // [ 'abc', 'a\nb\nc', 'ab\nac', 'a\na\na\na', 'b' ] -> 3+3+3+1+1
+  const uniqueAnswersPerGroup: UniqueAnswers[] = groups.map(groups2UniqueYesAnswers)
+  // 'abc' -> 3; 'a\nb\nc' -> 3;...
+  // [ 'abc', 'abc', 'abc', 'a', 'b' ] -> [ 3, 3, 3, 1, 1 ]
+  const countOfUniqueAnswersPerGroup: number[] = uniqueAnswersPerGroup.map(countUniqueYesAnswers)
+
+  // [ 3, 3, 3, 1, 1 ] -> 3+3+3+1+1 = 11
+  const result = sum(countOfUniqueAnswersPerGroup)
   
-  return 0
+  return result
 }
